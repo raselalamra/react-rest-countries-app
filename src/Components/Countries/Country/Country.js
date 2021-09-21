2f15f5975aed06a899c3877ef028e1d01db14b48
raselalamjubayed <raselalam838@gmil.com>
2021-09-22 01:54:46 +0600

    react added

---
+++ src/Components/Countries/Country/Country.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Country.css';
+
+const Country = (props) => {
+    const {name, flag, capital, population, region} = props.country
+    return (
+    
+        <div className="country">
+            <h3>This is: {name}</h3>
+            <img src={flag} alt="" />
+            <p><small>Region: {region}</small></p>
+            <p>Capital is: {capital} population: {population}</p>
+        </div>
+    );
+};
+
+export default Country;
